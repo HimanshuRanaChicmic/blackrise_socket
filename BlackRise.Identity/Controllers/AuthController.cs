b492f352367bf6a61302ab3e656acb51c390f951
DESKTOP-6L9CSPJ\Prachi <prachi.rajput752@chicmicstudios.in>
2025-07-10 13:44:00 +0530

    Apple login bug fix

--- BlackRise.Identity/Controllers/AuthController.cs
+++ BlackRise.Identity/Controllers/AuthController.cs
@@ -53,7 +53,8 @@ namespace BlackRise.Identity.Controllers
         {
             var linkedInCommand = new LinkedInCommand { AccessToken = code };
             var result = await _mediator.Send(linkedInCommand);
-            return Redirect($"{_clienturlSettings.LoginRedirect}?token={result.Token}&userId={result.UserId}&email={result.Email??""}&firstname={result.FirstName??""}&lastname={result.LastName??""}&isProfileCreated={result.IsProfileCreated?.ToString() ?? "false"}");
+            var isprofileCreated = result.IsProfileCreated == true ? "true" : "false";
+            return Redirect($"{_clienturlSettings.LoginRedirect}?token={result.Token}&userId={result.UserId}&email={result.Email??""}&firstname={result.FirstName??""}&lastname={result.LastName??""}&isProfileCreated={isprofileCreated}");
         }
 
         [HttpPost("login/google")]
